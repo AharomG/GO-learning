e906e033aa02d9a5206128e798a3029bc352aafa
AharomG <a.g.anaure@gmail.com>
2023-06-23 15:46:34 -0400

    test for and creation of packages

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"go-learning/testModule"
 	"io/fs"
 	"os"
 )
 
 func main() {
+	var testString string = "test"
 	var file *os.File
 	var err error
 	file, err = os.Open("values.txt")
@@ -22,4 +24,31 @@ func main() {
 	fmt.Println(err)
 	fmt.Println(numberOfReadBytes)
 	fmt.Println(string(buf))
+	test(&testString)
+	fmt.Println(testString)
+	testModule.TestFunction()
+	testFor()
+}
+
+func test(testString *string) {
+	*testString = "change"
+	fmt.Println(*testString)
+	test2(testString)
+}
+
+func test2(testString *string) {
+	*testString = "change2"
+	fmt.Println(*testString)
+}
+
+func testFor() {
+	var slice = []string{"hello", "this", "is a", "test"} //this is called a slice
+
+	for i := 0; i <= 5; i++ {
+		fmt.Println(i)
+	}
+
+	for _, elem := range slice {
+		fmt.Println(elem)
+	}
 }
---
+++ testModule/testFile.go
@@ -0,0 +1,7 @@
+package testModule
+
+import "fmt"
+
+func TestFunction() {
+	fmt.Println("This comes from another package")
+}
